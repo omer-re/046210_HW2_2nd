bdca8f936811a8d0f322eaef5e1ad49d339fc8d2
omer-re <1EkrV5mY>
2021-06-02 19:51:19 +0300

    tests passes

--- customized_kernel/kernel/list_manager.c
+++ customized_kernel/kernel/list_manager.c
@@ -7,35 +7,37 @@
 //// "global" list properties
 int files_paths_count;
 int privileged_procs_count;
-int was_initialized =0;
+int was_initialized = 0;
 list_t file_paths_list_head;
 
-void init_list(){
-    if (was_initialized==0){
+void init_list() {
+    if (was_initialized == 0)
+    {
         printk("init - list initializing\n");
         printk("init - run #\n");
         INIT_LIST_HEAD(&file_paths_list_head);
-        privileged_procs_count=0;
-        files_paths_count=0;
-        was_initialized=1;
+        privileged_procs_count = 0;
+        files_paths_count = 0;
+        was_initialized = 1;
     }
 }
-
-
-
 
 
 // free the list and all path entries
 void destroy_list() {
     init_list();
-    if (was_initialized==0){
+    if (was_initialized == 0)
+    {
         // list wasn't initialized yet, nothing to do
         return;
     }
     list_t *pos;
     list_t *temp;
-    list_for_each_safe(pos, temp, &file_paths_list_head){
-        Path_node_p a_node	= list_entry(pos, struct path_node, list_pointer);  // returns pointer to our struct
+    list_for_each_safe(pos, temp, &file_paths_list_head)
+    {
+        Path_node_p
+        a_node = list_entry(pos,
+        struct path_node, list_pointer);  // returns pointer to our struct
         list_del(pos); // delete list_t object
         kfree(a_node);  // free what we dynamically allocated
     }
@@ -47,32 +49,35 @@ void destroy_list() {
 
 // if change is 1 or -1 it's changing the counter.
 // if it's 0- it just returns the current count.
-int set_files_paths_count(int change){
-    files_paths_count+=change;
+int set_files_paths_count(int change) {
+    files_paths_count += change;
     return files_paths_count;
 }
 
 // if change is 1 or -1 it's changing the counter.
 // if it's 0- it just returns the current count.
-int set_privileged_procs_count(int change){
-    privileged_procs_count+=change;
+int set_privileged_procs_count(int change) {
+    privileged_procs_count += change;
     return privileged_procs_count;
 }
 
-
-int check_list_for_path(const char* pathName) {
-    if (was_initialized==0){
+int check_list_for_path(const char *pathName) {
+    if (was_initialized == 0)
+    {
         printk("SEARCH: list is empty\n");
         return 0;
     }
     list_t *pos;
-    list_for_each(pos, &file_paths_list_head){
+    list_for_each(pos, &file_paths_list_head)
+    {
         //Path_node_p itt= list_entry(pos,struct path_node, file_paths_list_head);
-        Path_node_p a_node	= list_entry(pos, struct path_node, list_pointer);  // returns pointer to our struct
+        Path_node_p
+        a_node = list_entry(pos,
+        struct path_node, list_pointer);  // returns pointer to our struct
 
 
 
-            if (!strcmp(a_node->file_path, pathName))
+        if (!strcmp(a_node->file_path, pathName))
         {
             printk("SEARCH: file is blocked \n");
             return 1;
--- customized_kernel/kernel/syscall_modifications.c
+++ customized_kernel/kernel/syscall_modifications.c
@@ -11,21 +11,24 @@
 #include <linux/list_manager.h>
 
 ////  restricted_syscall_open
-long restricted_syscall_open(const char * filename, int flags, int mode){
+long restricted_syscall_open(const char *filename, int flags, int mode) {
     init_list();
     //printk("sys_open\n");
-    int is_file_blocked=check_list_for_path(filename);
+    int is_file_blocked = check_list_for_path(filename);
 
-    if (is_file_blocked){   // file is in block list
+    if (is_file_blocked)
+    {   // file is in block list
 
         // check if process is privileged
-        if (current->is_privileged==0){
+        if (current->is_privileged == 0)
+        {
             // “EPERM” (Operation not permitted): The process is not privileged
             printk("sys_open: file is blocked, process isn't privileged, failed with EPERM\n");
             return -EPERM;
         }
-        // file is in list and process has permission
-        else if (current->is_privileged==1){
+            // file is in list and process has permission
+        else if (current->is_privileged == 1)
+        {
             printk("sys_open: file is blocked, process is privileged\n");
         }
     }
@@ -35,25 +38,27 @@ long restricted_syscall_open(const char * filename, int flags, int mode){
 
 
 ////  sys_block_clear
-int sys_block_clear(Path_node_p path_list_head){
+int sys_block_clear(Path_node_p path_list_head) {
 
     init_list();
     printk("block_clear: entered\n");
 
-    if (current->is_privileged!=1)
+    if (current->is_privileged != 1)
     {
         printk("block_clear: proc isn't privileged, can't clear list\n");
         return -EPERM;
     }
 
-    else if (set_privileged_procs_count(0)==0){
+    else if (set_privileged_procs_count(0) == 0)
+    {
         // no need to do nothing. no files.
         printk("block_clear: no need to do nothing. no files.\n");
 
         return 0;
     }
 
-    else if (set_files_paths_count(0)>0){
+    else if (set_files_paths_count(0) > 0)
+    {
         printk("block_clear: some files in list. free them.\n");
         destroy_list();
         return 0;
@@ -67,65 +72,75 @@ int sys_block_clear(Path_node_p path_list_head){
 }
 
 ////  sys_block_query
-int sys_block_query(const char *filename){
+int sys_block_query(const char *filename) {
     printk("block_query: entered\n");
 
     int res = check_list_for_path(filename);
-    if (res==0){
+    if (res == 0)
+    {
         printk("block_query: file doesn't exists\n");
     }
-    else if (res==1){
+    else if (res == 1)
+    {
         printk("block_query: file exists\n");
     }
     return res;
 }
 
 ////  sys_block_add_process
-int sys_block_add_process(pid_t pid){
+int sys_block_add_process(pid_t pid) {
     init_list();
     printk("sys_block_add_process\n");
-    printk("sys_block_add_process: pid entered- %d\n",pid);
-    printk("sys_block_add_process: current privileged procs number is %d \n",set_privileged_procs_count(0));
+    printk("sys_block_add_process: pid entered- %d\n", pid);
+    printk("sys_block_add_process: current privileged procs number is %d \n", set_privileged_procs_count(0));
 
     //   check if such pid exists
     //   if not -  return ESRCH
-    if (current==NULL){
+    if (current == NULL)
+    {
         printk("sys_block_add_process: current is NULL\n");
     }
-    if (current->is_privileged==1){
+    if (current->is_privileged == 1)
+    {
         task_t *pid_itt;
-        pid_itt= find_task_by_pid(pid);
-        if (pid_itt==NULL){
+        pid_itt = find_task_by_pid(pid);
+        if (pid_itt == NULL)
+        {
             // no matching pid
             printk("sys_block_add_process: such pid not exists\n");
             return -ESRCH;
         }
         printk("sys_block_add_process: LINE 100\n");
         // change permission
-        pid_itt->is_privileged=1;
+        pid_itt->is_privileged = 1;
         // increment counter
         set_privileged_procs_count(1);
         //return number of
-        printk("sys_block_add_process: operation allowed due to no other privileged procs %d\n",set_privileged_procs_count(0));
+        printk("sys_block_add_process: operation allowed due to no other privileged procs %d\n",
+               set_privileged_procs_count(0));
         return set_privileged_procs_count(0);
     }
-    // case of none privileged process, but no privileged processes
-    else if (current->is_privileged==0) {
+        // case of none privileged process, but no privileged processes
+    else if (current->is_privileged == 0)
+    {
         printk("sys_block_add_process: LINE 110\n");
 
         // check if privileged_procs_count==0
         // if (privileged_procs_count==0){
-        if (set_privileged_procs_count(0) == 0) {
+        if (set_privileged_procs_count(0) == 0)
+        {
             //      if true: allow operation
             task_t *pid_itt;
-            pid_itt= find_task_by_pid(pid);
+            pid_itt = find_task_by_pid(pid);
             pid_itt->is_privileged = 1;
             set_privileged_procs_count(1);
-            printk("sys_block_add_process: operation allowed due to no other privileged procs %d (should be 1)\n",set_privileged_procs_count(0));
+            printk("sys_block_add_process: operation allowed due to no other privileged procs %d (should be 1)\n",
+                   set_privileged_procs_count(0));
             return (set_privileged_procs_count(0));
         }
-        //      else: deny
-        else {
+            //      else: deny
+        else
+        {
             printk("sys_block_add_process: operation denied due to other privileged procs\n");
             return -EPERM;
         }
@@ -147,7 +162,8 @@ int sys_block_add_file(const char *filename) {
         printk("sys_block_add_file: filename is NULL\n");
         return -EFAULT;
     }
-    if (current->is_privileged==0){
+    if (current->is_privileged == 0)
+    {
         // if you aren't privileged you have nothing to do here.
         printk("sys_block_add_file: no permission to add file\n");
         return -EPERM;
@@ -161,19 +177,22 @@ int sys_block_add_file(const char *filename) {
         return 0;
     }
 
-    //  if needs to be added to the list
-    /////  CHECK PERMISSIONS
+        //  if needs to be added to the list
+        /////  CHECK PERMISSIONS
 
-    else if (current->is_privileged==1){
-        if(check_list_for_path(filename)==1){
+    else if (current->is_privileged == 1)
+    {
+        if (check_list_for_path(filename) == 1)
+        {
             // file already exists, do nothing, success
             printk("sys_block_add_file: filename is already exists, do nothing, success\n");
             return 0;
         }
         printk("sys_block_add_file: file isn't on list, add it.\n");
-        if(check_list_for_path(filename)==0){
+        if (check_list_for_path(filename) == 0)
+        {
             // allocate new (struct path_entry)=new_path_entry
-            Path_node_p new_path_node =(Path_node_p) kmalloc(sizeof(struct path_node), GFP_KERNEL);
+            Path_node_p new_path_node = (Path_node_p) kmalloc(sizeof(struct path_node), GFP_KERNEL);
 
             // check allocation
             if (new_path_node == NULL)
@@ -183,12 +202,13 @@ int sys_block_add_file(const char *filename) {
                 return -ENOMEM;
             }
             // initialize array
-            int i=0;
-            for (;i<PATH_MAX;i++){
-                new_path_node->file_path[i]='\0';
+            int i = 0;
+            for (; i < PATH_MAX; i++)
+            {
+                new_path_node->file_path[i] = '\0';
             }
             printk("sys_block_add_file: allocating successful\n");
-            if (copy_from_user(new_path_node->file_path, filename, strlen(filename)*sizeof(char)) != 0)
+            if (copy_from_user(new_path_node->file_path, filename, strlen(filename) * sizeof(char)) != 0)
             {
                 printk("write: Error on copying from user space.\n");
                 // if failed   “EFAULT” (Bad address): Error copying from user space, including if filename is NULL.
